3f28b43cd7c68ed89ad0ec3bc165eb70bc1983e4
Moon Antonio <pinchodev91@gmail.com>
2018-04-07 22:16:40 +0200

    Add Variables OnShadowEstado.cs

--- DES Project/Assets/DES Proyecto/Scripts/Debug/OnShadowEstado.cs
+++ DES Project/Assets/DES Proyecto/Scripts/Debug/OnShadowEstado.cs
@@ -1,20 +1,33 @@
 ﻿//                                  ┌∩┐(◣_◢)┌∩┐
 //																				\\
-// OnShadowEstado.cs (00/00/0000)													\\
+// OnShadowEstado.cs (07/04/2018)												\\
 // Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com			\\
-// Descripcion:																	\\
-// Fecha Mod:		00/00/0000													\\
-// Ultima Mod:																	\\
+// Descripcion:		Controlador de FPS											\\
+// Fecha Mod:		07/04/2018													\\
+// Ultima Mod:		Version Inicial												\\
 //******************************************************************************\\
+
 
 #region Librerias
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 #endregion
 
-namespace MoonAntonio
+namespace MoonAntonio.Debug
 {
+	[RequireComponent(typeof(Image))]
 	public class OnShadowEstado : MonoBehaviour 
 	{
+		#region Variables Publicas
+		public Image icono;
+		public Sprite spriteSinSombra;
+		public Sprite spriteEnSombra;
+		#endregion
+
+		#region Variables Privadas
+		[SerializeField] private bool onShadow = false;
+		#endregion
 
 	}
 }
