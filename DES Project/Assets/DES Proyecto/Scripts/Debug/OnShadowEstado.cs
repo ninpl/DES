0584125b3a7d9cffe2380aa837058406a0105196
Moon Antonio <pinchodev91@gmail.com>
2018-04-07 22:14:13 +0200

    Create OnShadowEstado.cs

---
+++ DES Project/Assets/DES Proyecto/Scripts/Debug/OnShadowEstado.cs
@@ -0,0 +1,20 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// OnShadowEstado.cs (00/00/0000)													\\
+// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com			\\
+// Descripcion:																	\\
+// Fecha Mod:		00/00/0000													\\
+// Ultima Mod:																	\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace MoonAntonio
+{
+	public class OnShadowEstado : MonoBehaviour 
+	{
+
+	}
+}
