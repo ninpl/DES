86bcd76e85ce540772c4f1e6621cea6e2c88e4a6
Moon Antonio <pinchodev91@gmail.com>
2018-04-07 22:30:15 +0200

    Add vars para el manager de materiales

---
+++ DES Project/Assets/DES Proyecto/Scripts/Unidad/ManagerMaterial.cs
@@ -0,0 +1,30 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// ManagerMaterial.cs (00/00/0000)													\\
+// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com			\\
+// Descripcion:																	\\
+// Fecha Mod:		00/00/0000													\\
+// Ultima Mod:																	\\
+//******************************************************************************\\
+
+#region Librerias
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#endregion
+
+namespace MoonAntonio
+{
+	public class ManagerMaterial : MonoBehaviour 
+	{
+		#region Variables Publicas
+		public MeshRenderer meshRenderer;
+		public SkinnedMeshRenderer skinnedMeshRenderer;
+		public Material[] materiales;
+		#endregion
+
+		#region Variables Privadas
+		private int id = 0;
+		#endregion
+	}
+}
