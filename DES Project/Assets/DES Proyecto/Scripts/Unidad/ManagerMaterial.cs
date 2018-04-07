ad54422da28c9a5740cae4275f44a9bafe936e4a
Moon Antonio <pinchodev91@gmail.com>
2018-04-07 22:32:18 +0200

    Funcionalidad de ManagerMaterial.cs

--- DES Project/Assets/DES Proyecto/Scripts/Unidad/ManagerMaterial.cs
+++ DES Project/Assets/DES Proyecto/Scripts/Unidad/ManagerMaterial.cs
@@ -26,5 +26,35 @@ namespace MoonAntonio
 		#region Variables Privadas
 		private int id = 0;
 		#endregion
+
+		#region Inicializadores
+		private void Start()
+		{
+			if (meshRenderer == null) meshRenderer = gameObject.GetComponent<MeshRenderer>();
+			if (meshRenderer == null && skinnedMeshRenderer == null) skinnedMeshRenderer = gameObject.GetComponent<SkinnedMeshRenderer>();
+		}
+		#endregion
+
+		#region Metodos Publicos
+		public void Next()
+		{
+			SetMaterial(id + 1);
+		}
+
+		public void Prev()
+		{
+			SetMaterial(id - 1);
+		}
+
+		public void SetMaterial(int id)
+		{
+			if (materiales == null || meshRenderer == null || materiales.Length == 0) return;
+
+			id = (id < 0) ? materiales.Length - 1 : (id > materiales.Length - 1) ? 0 : id;
+
+			if (meshRenderer != null) meshRenderer.material = materiales[id];
+			else skinnedMeshRenderer.material = materiales[id];
+		}
+		#endregion
 	}
 }
